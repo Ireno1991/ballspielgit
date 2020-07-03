59b61ef9cb829f49125c4544bf811ba3b6dc56a5
Ireno Wälte <ireno9@hotmail.com>
2020-07-03 09:04:14 +0200

    Projektdateien hinzufügen.

---
+++ ballspiel/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace BallSpiel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer _animationsTimer = new DispatcherTimer();
+        private bool gehtNachRechts = true;
+        private bool gehtNachUnten = true;
+
+        private int zaehler = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _animationsTimer.Interval = TimeSpan.FromMilliseconds(50);
+            _animationsTimer.Tick += PositioniereBall;
+        }
+
+        private void PositioniereBall(object sender, EventArgs e)
+        {
+            var x = Canvas.GetLeft(Ball);
+
+            if (gehtNachRechts)
+            {
+                Canvas.SetLeft(Ball, x + 5);
+            }
+            else
+            {
+                Canvas.SetLeft(Ball, x - 5);
+            }
+
+            if (x >= Spielplatz.ActualWidth - Ball.ActualWidth)
+            {
+                gehtNachRechts = false;
+            }
+            else if (x <= 0)
+            {
+                gehtNachRechts = true;
+            }
+
+            var y = Canvas.GetTop(Ball);
+
+            if (gehtNachUnten)
+            {
+                Canvas.SetTop(Ball, y + 5);
+            }
+            else
+            {
+                Canvas.SetTop(Ball, y - 5);
+            }
+
+            if (y >= Spielplatz.ActualHeight - Ball.ActualHeight)
+            {
+                gehtNachUnten = false;
+            }
+            else if (y <= 0)
+            {
+                gehtNachUnten = true;
+            }
+        }
+
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_animationsTimer.IsEnabled)
+            {
+                _animationsTimer.Stop();
+            }
+            else
+            {
+                _animationsTimer.Start();
+                zaehler = 0;
+                SpielstandLabel.Content = $"{zaehler} Clicks";
+            }
+        }
+
+        private void Ball_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_animationsTimer.IsEnabled)
+            {
+                zaehler += 1;
+                SpielstandLabel.Content = $"{zaehler} Clicks";
+            }
+        }
+
+        private void Ball_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.F:
+                    Ball.Fill = Brushes.Red;
+                    break;
+                case Key.Left:
+                    gehtNachRechts = false;
+                    break;
+                case Key.Right:
+                    gehtNachRechts = true;
+                    break;
+                case Key.Down:
+                    gehtNachUnten = true;
+                    break;
+                case Key.Up:
+                    gehtNachUnten = false;
+                    break;
+                default:
+                    // die anderen Tasten interessieren uns hier nicht!
+                    break;
+            }
+        }
+    }
+}
